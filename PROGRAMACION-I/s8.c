73c40253a1be0d14e4a7a1e49dc98a52bf7488e5
Alejomanzano <152665581+Alejomanzano@users.noreply.github.com>
2024-07-05 15:18:30 -0500

    Add files via upload

---
+++ PROGRAMACION-I/s8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "f.h"
+
+
+int main (void) {
+    int opcion;
+    //void leerArchivo();
+    //void guardarArchivo();
+    do {
+        printf("\n****\t1001 AUTOS\t****\n");
+        opcion = opcionmenu();
+        procesar(opcion); 
+    } while (opcion != 0);
+    return 0;   
+}
+
